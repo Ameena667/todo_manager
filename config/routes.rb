9760840e9420c6b489b01ced2ee1719670c5bcda
Ameena Shaik <n150667@rguktn.ac.in>
2021-12-28 12:20:31 +0530

    a single todo can be seen in /todo/:id

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #RESOURCE
+  #Todo is a resource
+  # - list of resources can be seen: index
+  # - single resource can be seen: show
+  # - create a new resource
   get "todos", to:"todos#index"
+  get "todos/:id", to:"todos#show"
 end
